2aeac7dbbeb4573ccdc64286bdf1fd511c447e7d
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2019-03-27 08:24:51 +0100

    re-arrange and comment

--- R/00_source.R
+++ R/00_source.R
@@ -1,11 +1,12 @@
 rm(list = ls())
-source("R/01_config.R")
-source("R/03_helperFunctions.R")
-source("R/04_simulateData.R")
-source("R/04_extractData.R")
-source("R/05_transformData.R")
-source("R/06_visualization.R")
+source("R/01_config.R")           # load packages, set constants, access credentials
+source("R/03_helperFunctions.R")  # define functions you'll need later on
+source("R/04_simulateData.R")     # simulate data if there is no real data to be analyzed
+source("R/04_extractData.R")      # extract some google analytics data using the API
+source("R/05_transformData.R")    # manipulate data
+source("R/06_visualization.R")    # visualize data
 
+# write a report/dashboard
 render(input = "R/99_flexDashboard.Rmd",
        flex_dashboard(theme = "bootstrap",
                       orientation = "rows",
@@ -18,4 +19,5 @@ render(input = "R/99_flexDashboard.Rmd",
        output_file = paste(Sys.Date(), "_TrafficReport", ".html",
                            sep = ""))
 
+# open the generated report/dashboard
 browseURL(paste("html/", Sys.Date(), "_TrafficReport", ".html", sep = ""))
