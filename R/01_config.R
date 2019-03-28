cfbe207e7d8a92d5c475e6a403a162e288b6e6f1
Hauke Roggenkamp <Hauke2Roggenkamp@gmail.com>
2019-03-28 13:46:46 +0100

    add global chunk options

--- R/01_config.R
+++ R/01_config.R
@@ -2,13 +2,6 @@
 # ** Constants -----
 set.seed(1234576)
 PUBLISHING_DATE <- "2019-02-15"
-START_DATE <- as.Date("2017-01-01")
-PLATFORMS <- c("Lights", "Furniture", "Misc")
-# I assume that we'll always have the data of the last full month available
-last_month_1 <- floor_date(Sys.Date() - months(1), "month")
-last_month_2 <- floor_date(Sys.Date() - months(2), "month")
-# determines when value box is colored (and icon-ed)
-V_BOX_THRESHOLD <- 0.025
 
 # ** Packages -----
 listOfPackages <- c("checkpoint",
@@ -41,6 +34,15 @@ if(!dir.exists("~/.checkpoint")){
 }
 
 checkpoint(PUBLISHING_DATE)
+
+# ** Project Specific Constants -----
+START_DATE <- as.Date("2017-01-01")
+PLATFORMS <- c("Lights", "Furniture", "Misc")
+# I assume that we'll always have the data of the last full month available
+last_month_1 <- floor_date(Sys.Date() - months(1), "month")
+last_month_2 <- floor_date(Sys.Date() - months(2), "month")
+# determines when value box is colored (and icon-ed)
+V_BOX_THRESHOLD <- 0.025
 
 # ** CI -----
 source("R/02_corporateDesign.R")
